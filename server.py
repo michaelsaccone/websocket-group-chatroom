df483ef6f98afa42f31538c0637c9e7a1038fdf6
Michael Saccone <michael@info0-00.cerebotani.it>
2024-05-11 12:44:07 +0200

    Add complete broadcast group chat

--- server.py
+++ server.py
@@ -2,13 +2,22 @@
 import websockets
 import asyncio
 
+clients = []
+accounts = {}
+
 async def handler(socket):
+    if socket not in clients:
+        clients.append(socket)
+        name = await socket.recv()
+        accounts[socket.remote_address[0]] = name
+        print("Connesso nuovo client: " + accounts[socket.remote_address[0]] + " con ip " + socket.remote_address[0])
+
     async for message in socket:
-        print("ricevuto " + str(message))
+        websockets.broadcast(clients, accounts[socket.remote_address[0]] + ": " + message)
 
 async def main():
     print("Server in ascolto sulla porta 3033 ... ")
-    async with websockets.serve(handler, "localhost", 3033):
+    async with websockets.serve(handler, "0.0.0.0", 3033):
         await asyncio.Future()  # loop infinito
 
 asyncio.run(main())
